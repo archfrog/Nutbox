4cf0273ee13640179af957f97d9fd80477914937
Mikael Lyngvig <mikael@lyngvig.org>
2015-03-04 00:59:35 +0100

    Updated the URL of the project.

--- src/Nutbox/Information.cs
+++ src/Nutbox/Information.cs
@@ -1,20 +1,20 @@
 #region license
 // Copyleft (-) 2009-2015 Mikael Lyngvig (mikael@lyngvig.org).  Donated to the Public Domain.
-// 
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following 
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
 // conditions are met:
-// 
+//
 //     * Redistributions of source code must retain the above copyright notice, this list of conditions and the disclaimer below.
-//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following 
+//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
 //       disclaimer in the documentation and/or other materials provided with the distribution.
-//     * Neither the name of Mikael Lyngvig nor the names of its contributors may be used to endorse or promote products derived 
+//     * Neither the name of Mikael Lyngvig nor the names of its contributors may be used to endorse or promote products derived
 //       from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
 // BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
 // SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
 // DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion
 
@@ -57,6 +57,13 @@ namespace Org.Nutbox
 			set { _support = value; }
 		}
 
+        private string _website;
+        public string Website
+        {
+            get { return _website; }
+            set { _website = value; }
+        }
+
 		private string _help;
 		public string Help
 		{
@@ -84,19 +91,21 @@ namespace Org.Nutbox
 			string company,
 			string rights,
 			string support,
+            string website,
 			string help,
 			int    lower,
 			int    upper
 		)
 		{
-			Program = program;
-			Version = version;
-			Company = company;
-			Rights  = rights;
-			Support = support;
-			Help    = help;
-			Lower   = lower;
-			Upper   = upper;
+			_program = program;
+			_version = version;
+			_company = company;
+			_rights  = rights;
+			_support = support;
+            _website = website;
+			_help    = help;
+			_lower   = lower;
+			_upper   = upper;
 		}
 	}
 }
